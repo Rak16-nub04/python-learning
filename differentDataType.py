e3f2117296646cd4408c9cfb385c141b7ff75aad
Rak16-nub04 <162906813+Rak16-nub04@users.noreply.github.com>
2024-09-10 21:46:29 +0600

    Add files via upload

---
+++ differentDataType.py
@@ -0,0 +1,26 @@
+#int type
+
+a=10
+
+print(a)
+
+#data type check
+print(type(a))
+
+#floating type data
+
+b=10.2
+
+print(b)
+
+print(type(b))
+
+#complex type data
+
+c=2j
+
+print(c)
+print(type(c))
+
+d="rakib"
+print("My name is"+' '+d)
---
+++ listtt.py
@@ -0,0 +1,46 @@
+#list type
+#list is mutable
+
+a=["rakib", "vlo", "manus"]
+
+
+print(a)
+
+print(type(a))
+
+anum=[1,2,3,4]
+print(anum)
+
+
+
+#tuple type
+#tuple is unmutable
+
+b=("rakib", "vlo", "manus")
+
+print(b)
+print(type(b))
+
+bnum=(1,2,3,4)
+print(bnum)
+
+#let's change the list
+
+a[0]="hablu"
+
+print(a)
+
+#range type data
+
+c=range(6)
+
+print(c)
+
+#using loop
+for i in c:
+    print(i)
+    
+
+
+
+
