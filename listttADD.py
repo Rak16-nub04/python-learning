126d14511801308aeb119a97a8dd75054cfbd472
Rak16-nub04 <162906813+Rak16-nub04@users.noreply.github.com>
2024-09-10 21:47:23 +0600

    Add files via upload

---
+++ listttADD.py
@@ -0,0 +1,23 @@
+a=["rakib", "NSU", "Dhaka"]
+
+print(a)
+
+#akta value add korte(last e add hobe)
+a.append("sisir")
+print(a)
+
+#fixed position e data add korte insert
+a.insert(2,"pagla")
+print(a)
+
+#practice
+
+r=["ki","kno","kivabe"]
+
+print(r)
+
+r.append("kothay")
+print(r)
+
+r.insert(1,"kobe")
+print(r)
---
+++ rmoveFromList.py
@@ -0,0 +1,22 @@
+a=["rakib","valo","manus", "nsu", "te ", "pore"]
+
+print(a)
+
+#remove use kore direct word likhte hobe
+a.remove("manus")
+
+print(a)
+
+#pop use korle index number dilei hobe
+a.pop(1)
+
+print(a)
+
+#del
+del a[0]
+print(a)
+
+#clear use korle puro list remove jhoye jabe
+a.clear()
+
+print(a)
